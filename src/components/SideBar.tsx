55b05e7ce27ba593af141ad42eb4e0fecc0e51f5
Gabriel Vanjura <gabrielvanjura@outlook.com>
2021-03-28 21:06:41 -0300

    Desafio Finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
+import { Button } from "./Button";
+interface SideBarProps {
+  onAlterGenreButton: (id: number) => void;
+  selectedGenreId: number;
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onAlterGenreButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
